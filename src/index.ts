465fbf11d57d9c225281e87d8e9df98d65046a83
Hugo Larrousse <hugo.larrousse@gmail.com>
2024-02-01 09:55:35 +0100

    Update package.json and add FieldInput component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import BaseAvatar from "./components/bases/BaseAvatar.vue";
 import BaseButton from "./components/bases/BaseButton.vue";
 import BaseIcon from "./components/bases/BaseIcon.vue";
 import FieldAvatarUploader from "./components/fields/FieldAvatarUploader.vue";
+import FieldInput from "./components/fields/FieldInput.vue";
 
 // CSS
 import "./assets/tailwind.css";
@@ -12,4 +13,5 @@ export {
   BaseButton,
   BaseIcon,
   FieldAvatarUploader,
+  FieldInput,
 };
